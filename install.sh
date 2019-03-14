a3d159a4cb7b266eea4106919a9bfcdb09c552ea
Lluís Pons <luisponshernandez@gmail.com>
2019-03-14 18:09:38 +0100

    Installation script

--- install.sh
+++ install.sh
@@ -1,9 +1,25 @@
-#!/bin/bash
+!/bin/bash
 
-# Install packages
+# Add apt repositories
+cat repositories.list | xargs -L1 apt-add-repository
+
+# Add apt GPG keys
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+wget -q -O - https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+wget -q -O - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+# Update apt and install packages
 sudo apt update
-sudo apt install $(cat dpkg/packagest.txt)
-sudo snap install $(cat snap/packagest.txt)
+sudo apt install $(cat dpkg/packagest.list)
+
+# Install snap packages
+sudo snap install $(cat snap/packages.list)
+
+#Install global Node Modules
+sudo npm install -g $(cat node/packages.list)
+
+#Make zsh default bash
+chsh -s $(which zsh)
 
 # Install Oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -11,14 +27,6 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 # Install powerlevel9k theme and fonts
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 sudo apt install fonts-powerline
-
-#Install Node
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install -y nodejs
-sudo npm install -g $(cat node/packages.txt)
-
-#Install nvm (Node version manager)
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
 # Install VS Code extensions
 cat vscode/extensions.list | grep -v '^#' | xargs -L1 code --install-extension
@@ -28,15 +36,12 @@ wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf - -C ~
 ~/.dropbox-dist/dropboxd
 
 # Setup vagrant
-vagrant plugin install install $(cat vagrant/plugins.txt)
+wget -c "https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.deb"
+sudo dpkg -i vagrant_2.2.4_x86_64.deb
+rm vagrant_2.2.4_x86_64.deb
+vagrant plugin install $(cat vagrant/plugins.list)
 mkdir ~/projectes
 git clone -b master git://github.com/Varying-Vagrant-Vagrants/VVV.git ~/vagrant-local
-
-# Install Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install docker-ce
 
 # Install Boostnote
 wget -c "https://github.com/BoostIO/boost-releases/releases/download/v0.11.15/boostnote_0.11.15_amd64.deb"
@@ -56,3 +61,4 @@ ln -sf "$DOTFILES_DIR/vscode/keybindings.json" ~/.config/Code/User/
 ln -sf "$DOTFILES_DIR/vscode/.eslintrc" ~
 ln -sf "$DOTFILES_DIR/.fonts" ~
 ln -sf "$DOTFILES_DIR/vagrant/vvv/vvv-custom.yml" ~/vagrant-local
+ln -sf "$DOTFILES_DIR/termiantor/config" ~/.config/terminator/
